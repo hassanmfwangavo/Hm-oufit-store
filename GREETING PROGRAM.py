47f8b4d300afa23fb78171156f8c811432386680
hassanmfwangavo <hmbusiness49@gmail.com>
2026-01-18 23:06:06 +0300

    Create GREETING PROGRAM.py
    This was a simple practical program that let user to enter his\her birth date then ouput displays  greeting word with the respective age

---
+++ GREETING PROGRAM.py
@@ -0,0 +1,10 @@
+print("-----greeting program---")
+
+#Variable declaration
+x=input("Enter your name ;")
+y=int(input("Enter your birth year "))
+
+
+z= 2026-y
+print("hello",x  +  "your age is",z)
+
